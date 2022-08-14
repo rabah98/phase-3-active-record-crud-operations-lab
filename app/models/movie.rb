0229fc13927f1ed0851fbdd5565b32b704195c94
Rabah <rmerzoug471@gmail.com>
2022-08-13 23:25:04 -0500

    commit tree c22f33a5eda6bfaeca39233a560df097c3c7a609

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,26 @@
 class Movie < ActiveRecord::Base
-
+    def self.create_with_title(title)
+        self.create(title: title)
+    end
+    def self.first_movie
+        self.first
+    end
+    def self.last_movie
+        self.last
+    end
+    def self.movie_count
+        self.last.id
+    end
+    def self.find_movie_with_id(id)
+        self.find(id)
+    end
+    def self.find_movie_with_attributes(attribute)
+        find_by(attribute)
+    end
+    def self.find_movies_after_2002
+        self.where("release_date > ?", 2002)
+    end
+    def self..update_all_titles updates(title)
+        self.update[title: title]
+    end
 end
